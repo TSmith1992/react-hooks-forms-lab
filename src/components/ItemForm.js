f0bf311d37a5380514507c823b62dc8701585b47
Tarique Smith <tpsmith1992@gmail.com>
2021-08-16 09:32:03 -0400

    commit tree e7ff166c9cbb38d096bd64a804e4c5e4b0051841

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+//ItemForm part: You need to add what the user puts in to the cart, complete with their category. 
+//Believe you will need to add event listeners to inputs and carry that info up to the App level,
+//(where the items are) and use the spread operator to add the item, so that new item set travels to the 
+//Shopping list
+function ItemForm({ addItems }) {
+  const [newItem, setNewItem]= useState({id: 1, name: "", category: "Produce"})
+
+  function newItemAdd(e){
+    let name = e.target.value
+    let category = e.target.value
+    if (e.target.name === "name"){
+      name = e.target.value
+    }
+    if (e.target.name === "category"){
+      category = e.target.value
+    }
+    const newItem = {
+      id: uuid(), // the `uuid` library can be used to generate a unique id
+      name: name,
+      category,
+    };
+    setNewItem(newItem)
+    addItems(newItem)
+  }
+
+  function onItemFormSubmit(e){
+    e.preventDefault()
+    newItemAdd(e)
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={onItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name"  value={newItem.name} onChange={newItemAdd}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={newItem.category} onChange={newItemAdd} >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,28 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, addItems }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [filterValue, setFilterValue] = useState("")
+
+  function onSearchChange(e){
+    setFilterValue(e.target.value)
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+    if (selectedCategory === "All" && filterValue === "") return true;
 
-    return item.category === selectedCategory;
+    return (item.category === selectedCategory || item.name === filterValue);
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm addItems={addItems}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={onSearchChange} filterValue={filterValue} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
